733eeaaba7d5f2c30d5ab21b47ef2aedaf4e2d38
Paul Molloy <2918590M@student.gla.ac.uk>
2024-03-09 11:31:28 +0000

    Fixed Navbar + fullscreen display

--- glasgowMusicAwards/static/javascript/script.js
+++ glasgowMusicAwards/static/javascript/script.js
@@ -2,8 +2,8 @@ function myFunction() {
     var x = document.getElementById("navList");
     var y = document.getElementById("navBar");
     if (x.className === "topNav") {
-      x.className = " responsive";
-      y.className = " greyNav"
+      x.className = "responsive";
+      y.className = "greyNav"
     } else {
       x.className = "topNav";
       y.className = "navBar"
